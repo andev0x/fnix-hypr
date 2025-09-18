90fff30758888b383e1ecda8e41514842ca2c710
andev0x <anvndev@gmail.com>
2025-09-18 23:02:42 +0700

    fix: hyprland conflict error

--- system/modules/services.nix
+++ system/modules/services.nix
@@ -3,10 +3,13 @@
 {
   services = {
   	dbus.enable = true;
-	picom.enable = true;
+	# picom is an X11 compositor and conflicts with Wayland compositors like Hyprland
+	# Disable to avoid black screen or double compositing
+	picom.enable = false;
 	openssh.enable = true;
     spice-vdagentd.enable = true;
 
+	# Enable X server for keyboard layout settings and SDDM, but prefer Wayland session
 	xserver = {
 		enable = true;
 		layout = "us, ru, ge";
@@ -15,6 +18,8 @@
 		displayManager = {
 			sddm.enable = true;
             sddm.theme = "${import ./sddm-theme.nix { inherit pkgs; }}";
+            # Use Wayland session for SDDM to launch Hyprland properly
+            sddm.wayland.enable = true;
 		};
 	};
   };
