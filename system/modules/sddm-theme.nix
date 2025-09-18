278e91dcd859178d6dc1328509d9f9ff301a5786
andev0x <anvndev@gmail.com>
2025-09-18 22:42:37 +0700

    feat(system): update 3 files

--- system/modules/sddm-theme.nix
+++ system/modules/sddm-theme.nix
@@ -3,7 +3,7 @@
 pkgs.stdenv.mkDerivation {
     name = "sddm-theme";
     src = pkgs.fetchFromGitHub {
-        owner = "andev";
+        owner = "andev0x";
         repo = "sddm-astronaut-theme";
         rev = "468a100460d5feaa701c2215c737b55789cba0fc";
         sha256 = "1h20b7n6a4pbqnrj22y8v5gc01zxs58lck3bipmgkpyp52ip3vig";
